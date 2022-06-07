6d6ef331f66fc7dd52a23342f2f3873b479df08a
Danny <Danielpradeep0@gmail.com>
2022-06-07 15:19:31 +0530

    first

---
+++ build/precache-manifest.e1c791a739aaf10b22bf727d683b5eaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5754a905a9231331562ab71b5c80b7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e2143249c90d0ef93ed",
+    "url": "/static/css/main.3fa44781.chunk.css"
+  },
+  {
+    "revision": "8e144dbb858c7fcbdcd4",
+    "url": "/static/js/2.752a4c61.chunk.js"
+  },
+  {
+    "revision": "a8de4128b679d6159aad8871b5569c6d",
+    "url": "/static/js/2.752a4c61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e2143249c90d0ef93ed",
+    "url": "/static/js/main.ccd36ae4.chunk.js"
+  },
+  {
+    "revision": "cf839f7d6a8ccd21a5db",
+    "url": "/static/js/runtime-main.c70acead.js"
+  }
+]);
